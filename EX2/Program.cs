48db5d99dcfa7555c1c85a3781bbf0476b6efa75
GregNan <ananev9295dt@gmail.com>
2022-07-10 11:18:13 +0300

    123:

---
+++ EX2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("vvedite imiy");
+string username = Console.ReadLine();
+Console.Write("Hello,");
+Console.Write(username);
+
+
