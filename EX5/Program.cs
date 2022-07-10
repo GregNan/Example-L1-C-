0dd856b7596afcad7292f05893ef1df4e5201eae
GregNan <ananev9295dt@gmail.com>
2022-07-10 12:55:33 +0300

    сделали встречное Маша

---
+++ EX5/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+    {
+        Console.Write("Ура, это же Маша");
+    }
+    else{
+        Console.Write("Привет ");
+        Console.Write(username);
+    }
+    
