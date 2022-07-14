4068b6bcda1aca9ef66f10637a768cfa8cab2b4f
GregNan <ananev9295dt@gmail.com>
2022-07-14 06:25:55 +0300

    module 1

---
+++ Module1/Program.cs
@@ -0,0 +1,65 @@
+﻿// программу вывода случайного значения из диапазона 
+//[10,99]
+// 1. Уточнения по отрезку
+//нужно ли считать дробные
+// 2. Блок-схема
+
+// int number = new Random().Next(10,100); //[10,100)
+
+// Console.WriteLine($"number: {number}");
+//int x = number/10;
+//int y = number%10;
+//if ( x > y )
+//{
+//    Console.WriteLine(x);
+//}
+//else
+//{
+//    Console.WriteLine(y);
+//}
+
+
+
+//Задача на массив. Массив из 8-ми элементов и вывести на экран
+//Уточнение 
+//буквы алфавита
+//Прописные или строчные
+// Случайные символы
+
+// Метод создания массива
+// Метод печати массива
+// метод получения случайного символа
+// Метод заполнения массива
+
+//Метод создания массива
+//строка 2гб. Война и мир 30мб
+char[] CreatArray(int count)
+{
+    return new char[count];
+}
+
+// Метод печати массива
+void Print(char[] array)
+{
+  int len = array.Length;
+  int i = 0;
+  while (i < len)
+  {
+    Console.WriteLine(array[i]);
+    i++;
+  } 
+}
+// Метод заполнения массива
+void Fill(char[] array)
+{
+    int length = array.Length;
+    int i=0;
+    while (i < length)
+    {
+        array[i]='!';
+        i++;
+    }
+}
+char[] arg = CreatArray(8);
+Fill(arg);
+Print(arg);
