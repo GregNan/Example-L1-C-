5e98adee5e3a97c1bc9e703e399649d36754f106
GregNan <ananev9295dt@gmail.com>
2022-07-15 06:24:14 +0300

    dz1

---
+++ Module1dz/Module1dz.cs
@@ -0,0 +1,43 @@
+﻿//Задача 10. Вторая цифра трёхзначного числа
+
+//int n=888;
+//int res;
+//if ( n < 0)
+//  n = -n;
+//while (n > 100)
+//  n = n/10;
+//res = n % 10; 
+//Console.WriteLine(res);
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//int n=69;
+//int res;
+//if (n<0)
+//    n=-n;
+//if (n>100)
+//  n= n/100;
+//  res=n%10;
+//Console.WriteLine(res);  
+//if (n<100)
+//Console.WriteLine("третьей цифры нет");  
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int weekday = Array[7];
+
+Console.WriteLine ("vved weekday:");
+Console.ReadLine(weekday);
+if (weekday > 0) 
+    weekday = 6;
+    weekday = 7;
+Console.Write ("vihodnoy");
+if (weekday > 0); 
+    weekday = 1;
+    weekday = 2;
+    weekday = 3;
+    weekday = 4;
+    weekday = 5;
+Console.Write ("budnie");
+
+
+
