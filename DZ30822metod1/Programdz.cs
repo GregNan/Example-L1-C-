846933019e393140bacfa5e487cba685731258be
GregNan <ananev9295dt@gmail.com>
2022-08-07 11:37:50 +0300

    dz 1.0

---
+++ DZ30822metod1/Programdz.cs
@@ -0,0 +1,21 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("vved a");
+int a = Console.Read();
+Console.WriteLine("vved a");
+int b = Console.Read();
+
+
+void Print (int[] array)
+{
+    int i=0;
+    int c = a;
+    for (i=0; i<b; i++)
+    c*=a;
+    i++;
+}
+
+
+Console.WriteLine($"chislo {c}");
