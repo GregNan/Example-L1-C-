8b26f8f714164b3f757ac29212a4eba16e9a02ea
GregNan <ananev9295dt@gmail.com>
2022-08-09 20:08:45 +0300

    ДЗ метод 1

--- DZ30822metod1/Programdz.cs
+++ DZ30822metod1/Programdz.cs
@@ -1,21 +1,52 @@
 ﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
-
-Console.WriteLine("vved a");
-int a = Console.Read();
-Console.WriteLine("vved a");
-int b = Console.Read();
+/*
+int k = Step(2,4);
+int n = Step (3,4);    
+Console.WriteLine($"chislo {k}");
+Console.WriteLine($"chislo {n}");
 
 
-void Print (int[] array)
+int Step(int a, int b)
 {
-    int i=0;
-    int c = a;
-    for (i=0; i<b; i++)
-    c*=a;
-    i++;
+    int c = 1;
+    for (int i=0; i < b; i++)
+    {
+        c*=a;
+    }
+       
+    return c;   
+}
+*/
+// Напишите метод, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/*int Tsep(int a)
+
+{   int c=0;
+    for (int i=0; i<3; i++)
+    {
+        int z=a % 10;
+        int a1=a/10;
+        int x=a1 % 10;
+        int a2= a1/10;
+        int h=a2%10;
+        int a3=a2/10;
+        int l=a3%10;
+        c=z+h+x+l;
+    }
+    return c;
+      
 }
 
+int r =Tsep(452);
+int t =Tsep(82);
+int y =Tsep(9012);
+Console.WriteLine(r);
+Console.WriteLine(t);
+Console.WriteLine(y);
+
+*/
 
 Console.WriteLine($"chislo {c}");
