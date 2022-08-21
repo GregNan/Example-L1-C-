b326e62542a605531c6ec8425e462c97195d9754
GregNan <ananev9295dt@gmail.com>
2022-08-21 22:36:26 +0300

    dz190822

---
+++ 160822seminar/Program.cs
@@ -0,0 +1,142 @@
+﻿//Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+/*
+Console.WriteLine ("Введите количество строчек двумерного массива");
+int row = Convert.ToInt(Console.ReadLine());
+Console.WriteLine ("Введите количество столбцов двумерного массива");
+int column = Convert.ToInt(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[row, column];
+int[,] array1 = new int[row, column];
+
+void PrintArray(int[,] arra2)
+{
+    for (int i=0; i< array2.GetLength(0);i++)
+    {
+      for (int j=0; j< array2.GetLength(1);j++)  
+      {
+        Console.Write(array2[i,j]+ " ");
+      }
+      Console.WriteLine();
+    }
+}
+void FillArray(int[,] array3)
+{
+    for (int i=0; i< array2.GetLength(0);i++)
+    {
+      for (int j=0; j< array2.GetLength(1);j++)  
+      {
+        Console.Write(array3[i,j]+ " ");
+      }
+      Console.WriteLine();
+    }
+}
+void ChangeRows (int[,] array4, int[,] array5)
+{
+    for(int index = 0; index < array4.GetLength(0); index++)
+    {
+        array[0, index] = array[row-1, index];
+        array5[row-1, index] = array4[0, index];
+    }
+}
+
+int[,] CopyArray(int[,] array)
+{
+    int[] t = new int[s.Length];
+    for (int i =0; i<s.Length; i++)
+    {
+        t[i]=s[i];
+    }
+    return t;
+}
+
+
+
+
+FillArray(array);
+array1= CopyArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeRows(array,array1);
+PrintArray(array1);
+*/
+//Палиндром
+
+//Console.WriteLine("vvedite chislo");
+/*int[] a = {1,6,3,4,5};
+int[] t= CopyArray(a);
+int[] CopyArray(int[] array)
+{
+    int[] t = new int[4];
+    for (int i =0; i<4; i++)
+    {
+        t[i]=a[4-i];
+    }
+    return t;
+}
+string s = Convert.ToString(a)!;
+string s1 = Convert.ToString(t)!;
+string s2,s3;
+
+if ((s2=s[myIndex0])==(s3=s1[myIndex0]))
+{
+   Console.WriteLine("Palindrom");
+}
+else
+{
+    Console.WriteLine("ne palindrom");
+} */
+static bool IsPalindrom(int x)
+{
+bool flag = true;
+string st = x.ToString();
+for (int i = 0; i < st.Length / 2; i++)
+flag &= st[st.Length - i - 1] == st[i];
+return flag;
+}
+Console.WriteLine("vvedite chislo");
+int x =Convert.ToInt32(Console.ReadLine());
+bool b = IsPalindrom(x);
+if (b==true)
+{
+ Console.WriteLine("pol");
+}
+if (b==false)
+{
+    Console.WriteLine("ne pol");
+}
+
+/*string s=Console.ReadLine()!;
+string s1;
+string Vozvrat(string arg)
+{
+for (int i=0; i< s.Length; i++)
+{
+ s1(i)=s(s.Length-i);
+ i++;
+}
+if (s==s1)
+{
+    Console.WriteLine("Palindrom");
+}
+else
+{
+    Console.WriteLine("ne palindrom");
+}
+}
+/*if (s[0] =s[4] && s[1]=s[3])
+{
+    Console.WriteLine("palindrom");
+}
+else
+{
+Console.WriteLine("ne palindrom");
+}
+Console.WriteLine();
+*/
+
+
+
+
+
+
---
+++ 190822seminar/Program.cs
@@ -0,0 +1,63 @@
+﻿/* n=до 30 000
+нужно вычислить разложение первых n
+чисел на простые множители*/
+
+//int fs =2; // 
+//              div   mod  mod==0  fs
+//int n=50;     //0       0           2
+              //n/fs    n%fs true   2
+              //
+/*for (int x=2; x<10; x++)
+{
+    int n=x;
+    string s = $"{x} = ";;
+    int fs = 2;
+    while (n != 1)
+    {
+        int mod = n%fs;
+        if (mod ==0)
+        {
+            n=n/fs;
+            s+= $"{fs}";
+            fs =2;
+        }
+        else
+        {
+            fs++;
+        }
+        Console.WriteLine($"s: {s}");
+    }
+}
+*/
+/*
+for (int x = 30000; x < 31000; x++)
+{
+  int n = x;
+  string s = $"{x} = "; ;
+  int fs = 2;
+  while (n != 1)
+  {
+    int mod = n % fs;
+    if (mod == 0)
+    {
+      n = n / fs;
+      s += $"{fs} ";
+      fs = 2;
+    }
+    else
+    {
+      fs++;
+    }
+  }
+  Console.WriteLine($"s: {s}");
+}его решение */ 
+string[] fnames = {"Ivan", "Sergey"};
+string[] lnames = {"Ivanov", "Sergeev"};
+
+for (int i=1; i<= 10; i++)
+
+{
+    int ni = Random.Shared.Next(fnames.Length);
+    int fi = Random.Shared.Next(lnames.Length);
+    File.AppendAllText("db1.csv", $"{i}; {fnames[ni]}; {lnames[fi]}; {Random.Shared.Next(10, 33)}\n");
+}
